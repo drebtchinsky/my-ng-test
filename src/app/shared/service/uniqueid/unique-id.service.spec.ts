d930aee3e9e171590d5da50bb92244ffaf4f8eed
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2021-07-30 16:38:30 -0300

    tests service

--- src/app/shared/service/uniqueid/unique-id.service.spec.ts
+++ src/app/shared/service/uniqueid/unique-id.service.spec.ts
@@ -1,16 +1,43 @@
-import { TestBed } from '@angular/core/testing';
-
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
+describe(UniqueIdService.name, () => {
   let service: UniqueIdService;
+  let ids = new Set();
+  const ptt = UniqueIdService.prototype;
+  const testSize = 50;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UniqueIdService);
+    service = new UniqueIdService();
+    ids = new Set();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it(`${ptt.generatedUniqueIdWithPrefix.name}
+  should be generate id when called`, () => {
+    const id = service.generatedUniqueIdWithPrefix('app');
+    expect(id.startsWith('app')).toBeTrue();
+  });
+
+  it(`${ptt.generatedUniqueIdWithPrefix.name}
+  should be generate different id when call again`, () => {
+    for (let i = 0; i < testSize; i++) {
+      ids.add(service.generatedUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(testSize);
+  });
+
+  it(`${ptt.getNumberOfGeneratedIds.name}
+  should return the number of generated ids when called`, () => {
+    for (let i = 0; i < testSize; i++) {
+      ids.add(service.generatedUniqueIdWithPrefix('app'));
+    }
+    expect(service.getNumberOfGeneratedIds()).toBe(testSize);
+  });
+
+  it(`${ptt.generatedUniqueIdWithPrefix.name}
+  should throw when called with empty`, () => {
+    const emptyValues = [null, undefined, ''];
+    emptyValues.forEach((value) => {
+      expect(() => service.generatedUniqueIdWithPrefix(value)).toThrow();
+    });
   });
 });
