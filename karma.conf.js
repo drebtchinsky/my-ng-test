f32b934b6021330c91ec93855aadb418ffe07327
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2021-07-31 23:37:42 -0300

    config karma custom launcher

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers:{
+      FirefoxSemCabeca:{
+        base:'Firefox',
+        flags:['-headless']
+      }
+    }
   });
 };
