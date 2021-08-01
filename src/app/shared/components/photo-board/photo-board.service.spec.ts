019b3294bb9d3743a36fee877908f3e1ccebf348
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2021-08-01 20:42:00 -0300

    test call api service with mock

--- src/app/shared/components/photo-board/photo-board.service.spec.ts
+++ src/app/shared/components/photo-board/photo-board.service.spec.ts
@@ -1,6 +1,59 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { PhotoBoardService } from './photo-board.service';
 
+const mockData = {
+  api: 'http://localhost:3000/photos',
+  data: [
+    {
+      id: 1,
+      description: 'example 1',
+      src: '',
+    },
+    {
+      id: 2,
+      description: 'example 2',
+      src: '',
+    },
+    {
+      id: 3,
+      description: 'example 3',
+      src: '',
+    },
+  ],
+};
+
 describe('PhotoBoardService', () => {
+  let service: PhotoBoardService;
+  let httpController: HttpTestingController;
+  const ptt = PhotoBoardService.prototype;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PhotoBoardService],
+    }).compileComponents();
+  });
+  beforeEach(() => {
+    service = TestBed.inject(PhotoBoardService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it(`#${ptt.getPhotos.name}
+    should return photos with description in upper case`, (done) => {
+    service.getPhotos().subscribe((photos) => {
+      photos.forEach((photo, i) => {
+        expect(photo.description)
+          .withContext(`mocked photo description ${i} `)
+          .toBe(photo.description.toUpperCase());
+      });
+      done();
+    });
+    httpController.expectOne(mockData.api).flush(mockData.data);
+  });
+
+  afterEach(() => httpController.verify());
 });
--- src/app/shared/components/photo-board/photo-board.service.ts
+++ src/app/shared/components/photo-board/photo-board.service.ts
@@ -2,7 +2,7 @@ import { Photo } from './photo-board';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 
 @Injectable()
 export class PhotoBoardService {
@@ -11,6 +11,13 @@ export class PhotoBoardService {
   getPhotos(): Observable<Photo[]> {
     return this.http
       .get<Photo[]>('http://localhost:3000/photos')
+      .pipe(
+        map((photos) =>
+          photos.map((photo) => {
+            return { ...photo, description: photo.description.toUpperCase() };
+          })
+        )
+      )
       .pipe(delay(2000));
   }
 }
