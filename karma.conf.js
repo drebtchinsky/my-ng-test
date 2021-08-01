8f0165803821e1cbce11f0ec305c033937026bc6
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2021-07-31 23:42:31 -0300

    config karm junit reporter

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
